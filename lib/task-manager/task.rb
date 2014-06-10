e5002148dad4536b0f6213b33254ee111669c91b
Mark Ellis <ellismarkf@gmail.com>
2014-06-10 11:10:54 -0500

    task can be marked as complete

--- lib/task-manager/task.rb
+++ lib/task-manager/task.rb
@@ -1,3 +1,15 @@
 
 class TM::Task
+  attr_reader :id, :description
+  attr_accessor :is_complete, :priority
+  @@counter = 0
+  def initialize id, description, priority = 0, is_complete=false
+    @id = @@counter += 1
+    @description = description
+    @priority = priority
+  end
+
+  def complete(id)
+    @is_complete = true
+  end
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,7 +1,28 @@
 require 'spec_helper'
 
 describe 'Task' do
+  let(:klass       ) { TM::Task }
+  let(:description ) { "write code" }
+  let(:is_complete ) { false }
+  let(:priority    ) { 0 }
+  let(:task        ) { klass.new(id, description, priority, is_complete) }
+  let(:id          ) { 1 }
+
+  # before each do
+  #   task = TM::Task.new()
+
   it "exists" do
     expect(TM::Task).to be_a(Class)
+  end
+
+  it "has an id, description, and priority number " do
+    expect(task.id).to eq(1)
+    expect(task.description).to eq('write code')
+    expect(task.priority).to eq(0)
+  end
+
+  it "can be marked as complete" do
+    task.complete(1)
+    expect(task.is_complete).to eq(true)
   end
 end

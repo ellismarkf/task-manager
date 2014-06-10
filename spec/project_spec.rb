0805c54aeba3f1cb80cd949dd4d25b281a260809
Mark Ellis <ellismarkf@gmail.com>
2014-06-10 10:22:49 -0500

    initialize project class with attributes

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,7 +1,20 @@
 require 'spec_helper'
 
 describe 'Project' do
+  let(:klass  ) { TM::Project }
+  let(:name   ) { "example" }
+  let(:project) { klass.new(name)}
+
   it "exists" do
-    expect(TM::Project).to be_a(Class)
+    expect(klass).to be_a(Class)
   end
+
+  it "initializes with a name" do
+    expect(project.name).to eq(name)
+  end
+
+  it "has a project id" do
+    expect(project.id).to eq(1)
+  end
+
 end
